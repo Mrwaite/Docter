f99603ea02c8ab721343054a12d21cde07da9b2c
Mrwaite <chenweite@xiyouant.org>
2016-03-09 22:40:06 +0800

    加了chat逻辑
    加了chat逻辑

--- html5_user_login/html5_user_login/assets/js/chat_page.js
+++ html5_user_login/html5_user_login/assets/js/chat_page.js
@@ -1,20 +1,70 @@
-window.setInterval("sendRequset('xxx.jsp')",1000);
-var id;
-function setAuto(){
-	id =setInterval("document.frames('chat').document.body.scroolTop = document.frames('chat').document.body.scroolHeight",1000);
-}
-function clearAuto(){
-	clearInterval(id);
+window.setInterval("showContent(docter_name,patient_name);",1000);
+
+
+function showContent(docter_name,patient_name){
+	$.ajax({
+		type ='GET',
+		url = 'xxx.jsp?docter_name=' +docter_name+ '&&patient_name=' + patient_name;
+		success:function(data){
+			var chat_content = JSON.parse(data);
+			var chat_content_div = '';
+			for(var i in chat_content){
+				if(chat_content[i]['profession'] === 'docter'){
+					chat_content_div += '<div class=\"box_left chat_tr\">\
+									        <img class=\"box_left\" src=\"assets/img/chat_img.png\" style=\"height: 50px;width: 50px;\"/>\
+										    <div>\
+										        <p class=\"chat_nick\">'+chat_content[i]['name']+'</p>\  
+											    <p class=\"chat_content arrow_left\">'+chat_content[i]['content']+'</p>\
+									        </div>\
+									    </div>';
+				}
+				else{
+					chat_content_div += '<div class=\"box_right chat_tr\">\
+									        <img class=\"box_right\" src=\"assets/img/chat_img.png\" style=\"height: 50px;width: 50px;\"/>\
+										    <div>\
+										        <p class=\"chat_nick\">'+chat_content[i]['name']+'</p>\  
+											    <p class=\"chat_content arrow_right\">'+chat_content[i]['content']+'</p>\
+									        </div>\
+									    </div>';
+				}
+				document.getElementById('chat_conent').innerHTML = chat_content_div;
+			}
+		}
+	});
+    document.getElementById('chat_conent').scrollTop = document.getElementById('chat_conent').scrollHeight*2;
 }
 
 
-function showChatMessage(){
-	var message = $.ajax({});
-	document.getElementById("chat").innerText = message;
-}
+$(document).ready(function() {
+    $('#chat_box>footer>form').submit(function(){
+        var new_chat_content = $(this).find('#submit_text').val();
+        var new_chat_content_div = '<div class=\"box_left chat_tr\">\
+									        <img class=\"box_left\" src=\"assets/img/chat_img.png\" style=\"height: 50px;width: 50px;\"/>\
+										    <div>\
+										        <p class=\"chat_nick\">'+ name +'</p>\  
+											    <p class=\"chat_content arrow_left\">'+new_chat_content+'</p>\
+									        </div>\
+									  </div>';
+		$('#chat_cotent').append(new_chat_content_div);
+		document.getElementById('chat_conent').scrollTop = document.getElementById('chat_conent').scrollHeight*2;							  
+        if(new_chat_content == '') {
+            return false;
+        }
+        
+		
+    });
 
-function refreshMessage(){
-    var timer = setInterval(showChatMessage,1000)
-}
+     $('form#submit_report').submit(function(){
+         var report_content = $('#input_about_evaluation').val();
+         if(report_content == ''){
+         	return false;
+         }
+     });
+
+
+
+});
+
+
 
 
